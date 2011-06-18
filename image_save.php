8d0ec13e34acb26b8ff17bd84d08ec5347787165
Jim <jimdoescode@gmail.com>
2011-06-18 15:21:31 -0400

    Added a simple php file to demonstrate how to save the image on a server

---
+++ image_save.php
@@ -0,0 +1,12 @@
+<?php
+
+$data = $_POST['imagedata'];
+$filename = 'jqScribbleImage.png';
+//Need to remove the stuff at the beginning of the string
+$data = substr($data, strpos($data, ",")+1);
+$data = base64_decode($data);
+$imgRes = imagecreatefromstring($data);
+if($imgRes !== false && imagepng($imgRes, $filename) === true)
+    echo "<img src='{$filename}' alt='jqScribble Created Image'/>";
+    
+?>
